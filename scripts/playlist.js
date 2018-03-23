57abddc2cb722621c3349631be2747343b44949f
jonaswalden <jonas.waldeen@gmail.com>
2018-03-23 23:41:44 +0100

    removed old m3u oriented files

--- scripts/playlist.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const Tracklist = require("./tracklist");
-
-module.exports = function Playlist (...args) {
-  const tracklist = Tracklist(...args);
-  return tracklist.join("\n");
-};
