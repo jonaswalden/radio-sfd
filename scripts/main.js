003ecd2f21c67790e2a49d23d5b349632368fc76
jonaswalden <jonas.waldeen@gmail.com>
2018-04-01 01:27:35 +0200

    get messages from g sheet

--- scripts/main.js
+++ scripts/main.js
@@ -89,7 +89,7 @@ function MusicPlayer (playlist) {
   }
 }
 
-function schedule ({items, vignetteAudio}, musicPlayer) {
+async function schedule ({itemsCsvUrl, vignetteAudio}, musicPlayer) {
   const audio = document.getElementById("alert-player__audio");
   const [text] = document.getElementsByClassName("alert-player__text");
   const [repeatButton] = document.getElementsByClassName("alert-player__repeat-button");
@@ -99,12 +99,33 @@ function schedule ({items, vignetteAudio}, musicPlayer) {
   const thisMonth = [date.getFullYear(), date.getMonth()];
   const today = [...thisMonth, date.getDate()];
   const tomorrow = [...thisMonth, today[2] + 1];
+  const messages = await getMessages();
+  console.log(messages);
   let [passedMessages, upcomingMessages] = sortMessages();
 
   queueNext();
 
   if (repeatButton) {
     repeatButton.addEventListener("click", repeatLast);
+  }
+
+  function getMessages () {
+    return fetch(itemsCsvUrl)
+      .then(data => data.text())
+      .then(CSVToArray)
+      .then(toItemList);
+
+    function toItemList (list) {
+      const [keys, ...rows] = list;
+      return rows.map(toObject);
+
+      function toObject (vals) {
+        return keys.reduce((obj, key, index) => {
+          obj[key] = vals[index];
+          return obj;
+        }, {});
+      }
+    }
   }
 
   function queueNext () {
@@ -168,7 +189,7 @@ function schedule ({items, vignetteAudio}, musicPlayer) {
   function sortMessages () {
     const now = Date.now();
 
-    return items
+    return messages
       .map(addTimeSize)
       .sort(byTimeSize)
       .reduce(passedAndUpcoming, [[], []]);
@@ -230,4 +251,53 @@ function setAudioSource (audio, src) {
       audio.dispatchEvent(new window.Event("ended"));
     }
   }
+}
+
+/**
+ * CSVToArray parses any String of Data including '\r' '\n' characters,
+ * and returns an array with the rows of data.
+ * @param {String} CSV_string - the CSV string you need to parse
+ * @param {String} delimiter - the delimeter used to separate fields of data
+ * @returns {Array} rows - rows of CSV where first row are column headers
+ */
+function CSVToArray (CSV_string, delimiter) {
+   delimiter = (delimiter || ","); // user-supplied delimeter or default comma
+
+   var pattern = new RegExp( // regular expression to parse the CSV values.
+     ( // Delimiters:
+       "(\\" + delimiter + "|\\r?\\n|\\r|^)" +
+       // Quoted fields.
+       "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+       // Standard fields.
+       "([^\"\\" + delimiter + "\\r\\n]*))"
+     ), "gi"
+   );
+
+   var rows = [[]];  // array to hold our data. First row is column headers.
+   // array to hold our individual pattern matching groups:
+   var matches = false; // false if we don't find any matches
+   // Loop until we no longer find a regular expression match
+   while (matches = pattern.exec( CSV_string )) {
+       var matched_delimiter = matches[1]; // Get the matched delimiter
+       // Check if the delimiter has a length (and is not the start of string)
+       // and if it matches field delimiter. If not, it is a row delimiter.
+       if (matched_delimiter.length && matched_delimiter !== delimiter) {
+         // Since this is a new row of data, add an empty row to the array.
+         rows.push( [] );
+       }
+       var matched_value;
+       // Once we have eliminated the delimiter, check to see
+       // what kind of value was captured (quoted or unquoted):
+       if (matches[2]) { // found quoted value. unescape any double quotes.
+        matched_value = matches[2].replace(
+          new RegExp( "\"\"", "g" ), "\""
+        );
+       } else { // found a non-quoted value
+         matched_value = matches[3];
+       }
+       // Now that we have our value string, let's add
+       // it to the data array.
+       rows[rows.length - 1].push(matched_value);
+   }
+   return rows; // Return the parsed data Array
 }
