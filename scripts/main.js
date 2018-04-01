208a56e5a9f7e2de676c81b14a39e805a59a1110
jonaswalden <jonas.waldeen@gmail.com>
2018-04-01 16:21:24 +0200

    resume playback from previous position

--- scripts/main.js
+++ scripts/main.js
@@ -16,19 +16,53 @@ function init () {
   const musicPlayer = MusicPlayer(playlist);
   mC(window.scheduleUrl, window.messages, musicPlayer);
   musicPlayer.start();
+  playlist.initCache(musicPlayer.audio);
 }
 
 function Playlist (tracks) {
   const amountTracks = tracks.length;
-  let t = amountTracks;
+  let currentTrackIndex = amountTracks + 1;
+  let cacheChecked = false;
 
   return {
-    next
+    next,
+    initCache
   };
 
   function  next () {
-    const track = tracks[t++ % amountTracks];
+    if (!cacheChecked) {
+      const cachedTrack = getFromCache();
+      if (cachedTrack) return cachedTrack;
+    }
+
+    const track = tracks[++currentTrackIndex % amountTracks];
     return track;
+  }
+
+  function getFromCache () {
+    cacheChecked = true;
+    const cache = localStorage.getItem("playingTrack");
+    localStorage.removeItem("playingTrack");
+    if (!cache) return;
+
+    const [trackIndexString, time] = cache.split("@");
+    currentTrackIndex = Number(trackIndexString);
+    const track = tracks[currentTrackIndex % amountTracks];
+
+    if (!track.includes("#t=")) return track + "#t=" + time;
+
+    let [path, anchor] = track.split("#t=");
+    const [start, end] = anchor.split(",");
+    return path + "#t=" + [time, end].filter(t => t).join(",")
+  }
+
+  function initCache (audio) {
+    window.addEventListener("beforeunload", cachePlayingTrack);
+
+    function cachePlayingTrack () {
+      console.log("track was", tracks[currentTrackIndex % amountTracks], audio.currentTime)
+      localStorage.setItem("playingTrack", currentTrackIndex + "@" + audio.currentTime);
+    }
   }
 }
 
@@ -47,6 +81,7 @@ function MusicPlayer (playlist) {
   audio.addEventListener("pause", toggleMusicState);
 
   return {
+    audio,
     start () {
       playNext();
       audio.addEventListener("ended", playNext);
