0823d385a85ec8cc4c6141daca7ca747ed07e1d5
jonas <jonas.waldeen@gmail.com>
2018-04-23 20:57:43 +0200

    eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 8
+  },
+  extends: "eslint:recommended",
+  rules: {
+    "no-cond-assign": "off"
+  },
+};
---
+++ scripts/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: false,
+  },
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 8,
+  }
+};
