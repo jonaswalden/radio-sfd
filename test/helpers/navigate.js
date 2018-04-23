0e66c646ba637d2b5bf10229cd3b2eb0e77ec1e6
jonas <jonas.waldeen@gmail.com>
2018-04-23 21:01:41 +0200

    multiple navigation options for unit testing

---
+++ test/helpers/navigate.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const fs = require("fs");
+const Tallahassee = require("@expressen/tallahassee");
+const {Compiler} = require("@expressen/tallahassee/lib/Compiler");
+
+module.exports = {
+  toFile,
+  toDomString,
+};
+
+function toFile (filepath) {
+  if (!filepath) return Promise.reject("no file path");
+
+  return new Promise((resolve, reject) => {
+    fs.readFile(filepath, "utf-8", (err, data) => {
+      if (err) return reject(err);
+
+      try {
+        resolve(Browser(data));
+      }
+      catch (err) {
+        reject(err);
+      }
+    });
+  });
+}
+
+function toDomString (domString) {
+  if (!domString) return Promise.reject("no dom string");
+
+  return new Promise((resolve, reject) => {
+    try {
+      resolve(Browser(domString));
+    }
+    catch (err) {
+      reject(err);
+    }
+  });
+}
+
+function Browser (text) {
+  Compiler([/scripts/]);
+  return Tallahassee({}).load({ text });
+}
