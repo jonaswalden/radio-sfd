688f4fc258ec3d73ed15377348cf8de7cecbad3f
jonas <jonas.waldeen@gmail.com>
2018-04-23 21:04:00 +0200

    better media interface

--- test/helpers/addMediaInterface.js
+++ test/helpers/addMediaInterface.js
@@ -2,8 +2,10 @@
 
 module.exports = function addMediaApi (element) {
   element.src = "";
+  element._playing = false;
   element.addEventListener("play", () => element._playing = true);
   element.addEventListener("ended", () => element._playing = false);
+  element.addEventListener("pause", () => element._playing = false);
   element.play = () => element.dispatchEvent("play");
-  element.pause = () => element._playing = false;
+  element.pause = () => element.dispatchEvent("pause");
 };
