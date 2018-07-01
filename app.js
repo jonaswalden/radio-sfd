7abab2ce156b610c15815c3ec189fa91b51a1252
jonas <jonas.waldeen@gmail.com>
2018-07-01 21:08:59 +0200

    start and test scripts work in parallell

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const serve = require('koa-static');
 const app = new Koa();
 app.use(serve('.'));
 
-const server = app.listen(4000);
+const port = process.env.port || 3000;
+const server = app.listen(port);
 
 module.exports = app;
 module.exports.stop = callback => server.close(callback);
