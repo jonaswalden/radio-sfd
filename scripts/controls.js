6d75f01f02f638a54ea1bf85f298f1ea5f6003c1
jonas <jonas.waldeen@gmail.com>
2018-08-14 00:15:20 +0200

    wip - fragmenting styles

--- scripts/controls.js
+++ scripts/controls.js
@@ -1,12 +1,12 @@
 export default function controls (musicPlayer, mc) {
   const [container] = document.getElementsByClassName('controls');
-  const [musicPauseButton, musicPlayButton, messageRepeatButton] = container.children;
+  const [pauseMusicButton, playMusicButton, repeatAlertButton] = container.children;
 
-  musicPauseButton.addEventListener('click', musicPlayer.playPause);
-  musicPlayButton.addEventListener('click', musicPlayer.playPause);
+  pauseMusicButton.addEventListener('click', musicPlayer.playPause);
+  playMusicButton.addEventListener('click', musicPlayer.playPause);
   window.addEventListener('keyup', KeyboardListener(' ', musicPlayer.pauseResume));
 
-  messageRepeatButton.addEventListener('click', mc.repeatLast);
+  repeatAlertButton.addEventListener('click', mc.repeatLast);
   window.addEventListener('keyup', KeyboardListener('backspace', mc.repeatLast));
 
   function KeyboardListener (which, callback) {
